5117ca656dc3a09b6dfd798123e9d01e3ec7ba65
TiLourinho <lourinho.tiago@gmail.com>
2022-07-29 16:17:08 -0300

    feat: schemas folder created

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,14 +1,7 @@
 import { z } from 'zod';
-
-const vehicleSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().gte(1900).lte(2022),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().int(),
-});
+import { vehicleSchema } from '../schemas';
 
 type IVehicle = z.infer<typeof vehicleSchema>;
 
-export { IVehicle };
 export default IVehicle;
+export { IVehicle };
---
+++ src/schemas/index.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+const vehicleSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+const carSchema = vehicleSchema.extend({
+  doorsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
+});
+
+export {
+  vehicleSchema,
+  carSchema,
+};

28cfa6bc12f775ddb43351c7c59cbc9dc43d01ef
TiLourinho <lourinho.tiago@gmail.com>
2022-07-30 12:12:05 -0300

    feat: endpoint GET /cars

--- src/routes/carRoute.ts
+++ src/routes/carRoute.ts
@@ -4,5 +4,6 @@ import carFactory from '../utils/factories';
 const carRoute = Router();
 
 carRoute.post('/', (req, res) => carFactory().create(req, res));
+carRoute.get('/', (req, res) => carFactory().read(req, res));
 
 export default carRoute;

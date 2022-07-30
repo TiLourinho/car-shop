88c1b52390a1b59d6f65e0dc0cc406d665202c16
TiLourinho <lourinho.tiago@gmail.com>
2022-07-30 18:49:25 -0300

    feat: endpoint PUT /cars/id

--- src/routes/carRoute.ts
+++ src/routes/carRoute.ts
@@ -6,5 +6,6 @@ const carRoute = Router();
 carRoute.post('/', (req, res) => carFactory().create(req, res));
 carRoute.get('/', (req, res) => carFactory().read(req, res));
 carRoute.get('/:id', (req, res) => carFactory().readOne(req, res));
+carRoute.put('/:id', (req, res) => carFactory().update(req, res));
 
 export default carRoute;

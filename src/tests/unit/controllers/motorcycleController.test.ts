4f16a7324753f24d37a92f40b028cdaacc092648
TiLourinho <lourinho.tiago@gmail.com>
2022-08-01 00:49:15 -0300

    test: motorcycleController read method

--- src/tests/unit/controllers/motorcycleController.test.ts
+++ src/tests/unit/controllers/motorcycleController.test.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 import MotorcycleModel from '../../../models/MotorcycleModel';
 import MotorcycleService from '../../../services/MotorcycleService';
 import MotorcycleController from '../../../controllers/MotorcycleController';
-import { motorcycleMock } from '../../mocks/motorcycleMocks';
+import { motorcycleMock, allMotorcyclesMock } from '../../mocks/motorcycleMocks';
 
 const { expect } = chai;
 
@@ -33,6 +33,24 @@ describe('6 - MotorcycleController', () => {
       await motorcycleController.create(req, res);
       expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
       expect((res.json as sinon.SinonStub).calledWith(motorcycleMock)).to.be.true;
+    });
+  });
+
+  describe('Read method', () => {
+    before(() => {
+      sinon.stub(motorcycleService, 'read').resolves(allMotorcyclesMock);
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('tests if "read" has status 200 and returns an object exactly equal to "allMotorcyclesMock"', async () => {
+      await motorcycleController.read(req, res);
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(allMotorcyclesMock)).to.be.true;
     });
   });
 });

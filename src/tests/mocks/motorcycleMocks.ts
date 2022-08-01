4b21ea381befc9269f5f9d5e354a58b283b7317e
TiLourinho <lourinho.tiago@gmail.com>
2022-08-01 00:47:13 -0300

    test: motorcycleModel read method

--- src/tests/mocks/motorcycleMocks.ts
+++ src/tests/mocks/motorcycleMocks.ts
@@ -19,7 +19,29 @@ const motorcycleMockWithId: IMotorcycle & { _id: string }= {
   engineCapacity: 1000
 };
 
+const allMotorcyclesMock: IMotorcycle[] & { _id: string }[] = [
+	{
+		_id: '62e721d8fb384e37fa8e7f2e',
+    model: "Honda CBR 1000RR",
+    year: 2007,
+    color: "black",
+    buyValue: 37900,
+    category: "Street",
+    engineCapacity: 1000
+	},
+	{
+		_id: '62e72ec0da4b869355d9976c',
+    model: "Kawasaki Ninja ZX-9R",
+    year: 1999,
+    color: "green",
+    buyValue: 25000,
+    category: "Street",
+    engineCapacity: 900
+	}
+];
+
 export {
   motorcycleMock,
-  motorcycleMockWithId
+  motorcycleMockWithId,
+  allMotorcyclesMock
 };
--- src/tests/unit/models/motorcycleModel.test.ts
+++ src/tests/unit/models/motorcycleModel.test.ts
@@ -2,7 +2,7 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 import { Model } from 'mongoose';
 import MotorcycleModel from '../../../models/MotorcycleModel';
-import { motorcycleMock, motorcycleMockWithId } from '../../mocks/motorcycleMocks';
+import { motorcycleMock, motorcycleMockWithId, allMotorcyclesMock } from '../../mocks/motorcycleMocks';
 
 const { expect } = chai;
 
@@ -26,6 +26,34 @@ describe('4 - MotorcycleModel', () => {
     it('tests if "create" returns an object exactly equal to "motorcycleMockWithId"', async () => {
       const newMotorcycle = await motorcycleModel.create(motorcycleMock);
       expect(newMotorcycle).to.be.deep.equal(motorcycleMockWithId);
+    });
+  });
+
+  describe('Read method', () => {
+    before(() => {
+      sinon.stub(Model, 'find')
+        .onCall(0).resolves(allMotorcyclesMock)
+        .onCall(1).resolves([])
+        .onCall(2).resolves(allMotorcyclesMock);
+    });
+
+    after(()=>{
+      sinon.restore();
+    });
+
+    it('tests if "read" returns an array', async () => {
+      const allMotorcycles = await motorcycleModel.read();
+      expect(allMotorcycles).to.be.an('array');
+    });
+
+    it('tests if "read" returns an empty array when database has no registered motorcycles', async () => {
+      const allMotorcycles = await motorcycleModel.read();
+      expect(allMotorcycles).to.be.deep.equal([]);
+    });
+
+    it('tests if "read" returns an array exactly equal to "allMotorcyclesMock"', async () => {
+      const allMotorcycles = await motorcycleModel.read();
+      expect(allMotorcycles).to.be.deep.equal(allMotorcyclesMock);
     });
   });
 });

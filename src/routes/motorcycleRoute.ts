8b53868d7842c43dbdcc35168ea91a2fca4325cb
TiLourinho <lourinho.tiago@gmail.com>
2022-08-01 12:31:46 -0300

    fix: route syntax

--- src/routes/motorcycleRoute.ts
+++ src/routes/motorcycleRoute.ts
@@ -5,8 +5,7 @@ const motorcycleRoute = Router();
 
 motorcycleRoute.post('/', (req, res) => motorcycleFactory().create(req, res));
 motorcycleRoute.get('/', (req, res) => motorcycleFactory().read(req, res));
-motorcycleRoute.get('/:id', (req, res) => { 
-  motorcycleFactory().readOne(req, res);
-});
+motorcycleRoute.get('/:id', (req, res) => 
+  motorcycleFactory().readOne(req, res));
 
 export default motorcycleRoute;
